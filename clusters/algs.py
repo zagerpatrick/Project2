cf0b2354f9c05880fe737ebd0f80cd8bd27ae8ea
Patrick Zager <zagerpatrick@gmail.com>
2021-02-06 22:29:00 -0800

    added Ligand class and ligand_reader function

--- clusters/algs.py
+++ clusters/algs.py
@@ -1,4 +1,116 @@
+from csv import reader
 import numpy as np
+
+
+def ligand_reader(file_name):
+    '''
+    Reads .csv file of the following format and imports individual ligands
+    into the Ligand class.
+
+    Format:
+    | Ligand ID | Score | SMILES | OnBits |
+    |     0     |   10  |  OC#N  | 53,624 |
+
+    Parameters
+    ----------
+    ligand_id : string
+        Path to .csv file.
+
+    Returns
+    ----------
+    ligand_list : list of ligand class instances
+        List of instances of the ligand class from .csv file.
+    '''
+    ligand_list = []
+
+    with open(file_name) as file:
+        next(file)
+        for line in reader(file):
+            ligand_id = int(line[0])
+            score = float(line[1])
+            smiles = line[2]
+            onbits = [int(i) for i in line[3].split(',')]
+            ligand_list.append(Ligand(ligand_id, score, smiles, onbits))
+
+    return ligand_list
+
+
+class Ligand():
+    '''
+    Class for ligand data containing id's, docking scores, molecular id's,
+    and ECFP onbits.
+    
+    This class also contains the function to transform onbits from sting 
+    format to np.array format.
+    
+    
+    Parameters
+    ----------
+    ligand_id : int
+        Numerical indentification of ligands. 
+    score : float
+        Autodock Vina score.
+    smiles : string
+        A chemical species string in simplified molecular-input 
+        line-entry system (SMILES) format.
+    onbits : list of ints
+        Extended-Connectivity Fingerprints (ECFP) characterization.
+
+    Attributes
+    ----------
+    ligand_id_ : int
+        Numerical indentification of ligands. 
+    score_ : float
+        Autodock Vina score.
+    smiles_ : string
+        A chemical species string in simplified molecular-input 
+        line-entry system (SMILES) format.
+    onbits_ : list of ints
+        Extended-Connectivity Fingerprints (ECFP) characterization.
+    onbits_array : np.array
+        Long form ECFP binary array of shape [n, 1024].
+        
+    Notes
+    -----
+    ...
+
+    References
+    ----------
+    1. Weininger, David. "SMILES, a chemical language and information system. 
+    1. Introduction to methodology and encoding rules." Journal of chemical 
+    information and computer sciences 28.1 (1988): 31-36.
+    
+    2. Rogers, David, and Mathew Hahn. "Extended-connectivity fingerprints."
+    Journal of chemical information and modeling 50.5 (2010): 742-754.
+
+
+    Examples
+    --------
+    >>> Ligand.ligand_id_ 
+    8 
+    >>> Ligand.score_
+    -2.6
+    >>> Ligand.smiles_
+    'O=C(C)C'
+    >>> Ligand.onbits_
+    [4, 6, 33, 397, 650, 807, 893, 1017]
+    >>> Ligand.onbits_array_[0, 0:10]
+    array([0., 0., 0., 0., 1., 0., 1., 0., 0., 0.])
+    '''
+    def __init__(self, ligand_id, score, smiles, onbits):
+        self.ligand_id_ = ligand_id
+        self.score_ = score
+        self.smiles_ = smiles
+        self.onbits_ = onbits
+        self.onbits_array_ = []
+    
+    def list2array(self):
+        '''
+        Convert onbits list to onbits binary array.
+        '''
+        onbits_array = np.zeros(1024)
+        onbits_array[self.onbits_] = 1
+        self.onbits_array_ = onbits_array
 
 
 def jaccard_dis(x, c):
@@ -24,21 +136,22 @@ def jaccard_dis(x, c):
 
     m11 = np.dot(x, c.T)
     jd = 1 - (m11/tot)
-    
+
     return jd
 
 
 class HierarchicalClustering():
 	pass
 
+
 class PartitionClustering():
     '''
-    ***Under Construction***
+    ***Under Construction***j
     K-means clustering
     '''
-    
+
     def kmeans(data, n_clusters):
-    
+
         # Determine dimmensions of input matrix
         m, n = data.shape[0], data.shape[1]
 
